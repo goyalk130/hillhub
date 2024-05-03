d2136865a9f9622c61a11fe72799d69aa43e3b98
“Karan <goyalkaran130@gmail.com>
2024-05-03 23:28:41 +0530

    added logout button

--- js/index.js
+++ js/index.js
@@ -17,6 +17,10 @@ let loginsignupbtn = document.getElementById("loginb");
 let dashboard = document.getElementById("dashboard");
 let addbutton = document.getElementById("add-items");
 
+let loggedInUserEle = document.querySelector(".looged-in-user");
+let logoutButton = document.getElementById("logout")
+
+loggedInUserEle.innerHTML = getCurrentUser();
 if (localStorage.getItem("CURRENT_LOGGED_IN_USER") == null) {
   localStorage.setItem("CURRENT_LOGGED_IN_USER", "");
 }
@@ -25,9 +29,18 @@ function checkuser() {
   if (getCurrentUser() != "") {
     dashboard.style.display = "flex";
     home.style.display = "none";
+  }else{
+    dashboard.style.display = "none";
+    home.style.display = "block";
   }
 }
 checkuser();
+
+
+logoutButton.addEventListener("click",()=>{
+      localStorage.setItem("CURRENT_LOGGED_IN_USER","")
+      checkuser()
+})
 
 async function logMovies() {
   const url = "https://ott-details.p.rapidapi.com/search?title=dilwale&page=1";
@@ -48,14 +61,14 @@ async function logMovies() {
   }
 }
 
-addbutton.addEventListener("click", (e) => {
-  let newDiv = document.createElement("div");
-  let text = document.createTextNode("Hi , how are you");
+// addbutton.addEventListener("click", (e) => {
+//   let newDiv = document.createElement("div");
+//   let text = document.createTextNode("Hi , how are you");
 
-  newDiv.appendChild(text);
-  dashboard.appendChild(newDiv);
-  logMovies();
-});
+//   newDiv.appendChild(text);
+//   dashboard.appendChild(newDiv);
+//   logMovies();
+// });
 
 function getCurrentUser() {
   return localStorage.getItem("CURRENT_LOGGED_IN_USER");
@@ -78,6 +91,7 @@ function loginOrSignup(data) {
         if (user.pass == data.pass) {
           console.log("password matched");
           localStorage.setItem("CURRENT_LOGGED_IN_USER", user.user);
+
           checkuser();
         } else {
           alert("Wrong pass");
